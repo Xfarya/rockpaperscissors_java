35ae8e401aa4138938c1480fff5642976101e356
Xfarya <farya243@gmail.com>
2022-04-19 17:27:04 +0100

    added tests for all scenarios of rock paper scissors, but only with manual entry of opponent's move

--- src/main/java/com/example/rps/RPS.java
+++ src/main/java/com/example/rps/RPS.java
@@ -2,17 +2,36 @@ package com.example.rps;
 
 public class RPS {
 
-    public static String opponentMove = "scissors";
     public static String result;
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 
-    public static String rockVScissors(String rock) {
-       if (opponentMove == "scissors") {
-           result = "You win";
+    public static String result(String myMove, String opponentMove) {
+        if (opponentMove.equals(myMove)) {
+            result = "You tied!";
+        } else if (opponentMove.equals("scissors")) {
+            result = "You win";
+        } else if (opponentMove.equals("paper")) {
+            result = "You lose";
+        } else {
+            if (myMove.equals("paper") && opponentMove.equals("rock")) {
+                result = "You win";
+            } else if (myMove.equals("paper") && opponentMove.equals("scissors")) {
+                result = "You lose";
+            } else if (myMove.equals("paper") && opponentMove.equals("paper")) {
+                result = "You tied!";
+            } else {
+                if (myMove.equals("scissors") && opponentMove.equals("paper")) {
+                    result = "You win";
+                } else if (myMove.equals("scissors") && opponentMove.equals("rock")) {
+                    result = "You lose";
+                } else if (myMove.equals("scissors") && opponentMove.equals("scissors")) {
+                    result = "You tied!";
+                }
+            }
         }
-       return result;
+            return result;
     }
 }
--- src/test/java/com/example/rps/RPSTest.java
+++ src/test/java/com/example/rps/RPSTest.java
@@ -7,10 +7,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RPSTest {
-    String opponentMove = "scissors";
 
+// all rock tests
     @Test
     public void rockBeatsScissors() {
-        assertEquals("You win", RPS.rockVScissors("rock"));
+        assertEquals("You win", RPS.result("rock", "scissors"));
     }
+
+    @Test
+    public void rockLosesPaper() {
+        assertEquals("You lose", RPS.result("rock", "paper"));
+    }
+
+    @Test
+    public void rockTiesRock() {
+        assertEquals("You tied", RPS.result("rock", "rock"));
+    }
+// all paper tests
+    @Test
+    public void paperBeatsRock() {
+        assertEquals("You win", RPS.result("paper", "rock"));
+    }
+
+    @Test
+    public void paperLosesScissors() {
+        assertEquals("You lose", RPS.result("paper", "scissors"));
+    }
+
+    @Test
+    public void paperTiesPaper() {
+        assertEquals("You tied!", RPS.result("paper", "paper"));
+    }
+
+//all scissors tests
+@Test
+    public void scissorsBeatPaper() {
+    assertEquals("You win", RPS.result("paper", "rock"));
+}
+
+    @Test
+    public void scissorsLosesRock() {
+        assertEquals("You lose", RPS.result("paper", "scissors"));
+    }
+
+    @Test
+    public void scissorsTiesScissors() {
+        assertEquals("You tied!", RPS.result("paper", "paper"));
+    }
+
 }
