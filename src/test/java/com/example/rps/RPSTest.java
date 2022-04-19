e1348c9727e511414c0b5038b794d903da4e910d
Xfarya <farya243@gmail.com>
2022-04-19 16:43:19 +0100

    added initial set up of RPS project with test suite

---
+++ src/test/java/com/example/rps/RPSTest.java
@@ -0,0 +1,12 @@
+package com.example.rps;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RPSTest {
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
